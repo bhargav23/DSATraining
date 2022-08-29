2a5eb7c81bccf04e8e208e6d4d185373952fffcd
bhargav23 <bhargav.kiran@gmail.com>
2022-08-29 17:04:14 +0530

    Create LinkedList1.java

---
+++ LinkedList1.java
@@ -0,0 +1,113 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+class Node {
+    private int data;
+    private Node next;
+    public Node (int data){
+        this.data = data;
+    }
+    public void setData(int data) {
+        this.data = data;
+    }
+    public void setNext(Node node){
+        this.next=node;
+    }
+    public int getData() {
+        return this.data;
+    }
+    public Node getNext(){
+        return this.next;
+    }
+}
+
+class LinkedList {
+    private Node head;
+    private Node tail;
+    public Node getHead(){
+        return this.head;
+    }
+    public Node getTail(){
+        return this.tail;
+    }
+    public void addAtEnd(int data){
+        Node node = new Node(data); 
+        if(this.head==null){
+            this.head = this.tail =node;
+        } else {
+            this.tail.setNext(node);
+            this.tail = node;
+        }
+    }
+    /* 
+
+
+1. Create a new node with the data passed to the method
+
+2. If the linked list is empty, make the new node as the head and the tail node.
+
+3. If the linked list is not empty, add the new node at the beginning, 
+i.e., before the current head node and make the new node as the head.
+    
+    */
+    public void addAtBeginning(int data){
+        Node node = new Node(data);
+      if(this.head==null) {
+        this.head = this.tail = node;
+      } else {
+            node.setNext(this.head);
+            this.head = node;
+           
+      }
+    }
+    public void display(){
+        Node temp = this.head;
+        while(temp!=null) {
+            System.out.println(temp.getData());
+            temp = temp.getNext();
+        }
+    }
+    /*
+    public void inset(int data, int before){
+        
+    }
+    public void delete(int data) {
+        
+    } 
+    
+    find(data)
+1.Take a temp reference and assign it with head node
+2.While temp is not null,
+  a.If the data at temp is equal to the data being searched for then, return temp
+  b.Else, make the next node as temp
+3.If data is not found then, return null
+    */
+    
+    public Node find(int data) {
+        Node temp = this.head;
+        while(temp!=null) {
+            if(temp.getData()==data) {
+                return temp;
+            } else {
+                temp = temp.getNext();
+            }
+        }
+        return null;
+    }
+}
+class LL {
+  public static void main (String[] args) {
+    LinkedList list = new LinkedList();
+        list.addAtEnd(10);
+        list.addAtEnd(20);
+        list.addAtBeginning(5);
+        list.display();
+        Node f = list.find(3);
+        if(f==null) 
+            System.out.println("Not found");
+        else
+            System.out.println(f.getData());
+        
+        
+  }
+}
